6fa9cccc8a0fc55a06d5768b08a2db63bd18a9ef
rstriklwlg <62120196+rstriklwlg@users.noreply.github.com>
2020-03-12 19:08:19 -0400

    Update agent_wrapper_installer.sh

--- agents-linux/agent_wrapper_installer.sh
+++ agents-linux/agent_wrapper_installer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cd "$PWD/agents-linux/"
 mkdir "$PWD/agents-linux/"
-curl https://github.com/rstriklwlg/Install/blob/master/agents-linux/agent_installer.sh
-curl https://github.com/rstriklwlg/Install/blob/master/agents-linux/cafile.pem
-curl https://github.com/rstriklwlg/Install/blob/master/agents-linux/client.crt
-curl https://github.com/rstriklwlg/Install/blob/master/agents-linux/client.key
-curl https://github.com/rstriklwlg/Install/blob/master/agents-linux/config.json
+cd "$PWD/agents-linux/"
+curl -OL https://github.com/rstriklwlg/Install/blob/master/agents-linux/agent_installer.sh
+curl -OL https://github.com/rstriklwlg/Install/blob/master/agents-linux/cafile.pem
+curl -OL https://github.com/rstriklwlg/Install/blob/master/agents-linux/client.crt
+curl -OL https://github.com/rstriklwlg/Install/blob/master/agents-linux/client.key
+curl -OL https://github.com/rstriklwlg/Install/blob/master/agents-linux/config.json
 chmod +x agent_installer.sh
 sh ./agent_installer.sh install_start
 systemctl enable ir_agent.service
